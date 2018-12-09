71f3b86cdd819bdf11f39a522cdf59db541c47ba
MikKarl <mikkokk@metropolia.fi>
2018-12-09 16:13:17 +0200

    get event data from server

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-event-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
-  constructor() { }
+  events: Object;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getEvents().subscribe( data => {
+      this.events = data
+      console.log(this.events)
+    })
   }
-
 }
