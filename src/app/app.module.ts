610685e7ca89e4f9775c2a2b210c7e7aaca305e6
roosamm <roosamm@metropolia.fi>
2018-12-10 14:46:23 +0200

    merge fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
@@ -9,12 +8,9 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { CreateModalComponent } from './create-modal/create-modal.component';
 import { WarnModalComponent } from './warn-modal/warn-modal.component';
-<<<<<<< HEAD
 import { EventsComponent } from './events/events.component';
 import { NewComponent } from './new/new.component';
 import { GuidelinesComponent } from './guidelines/guidelines.component';
-=======
->>>>>>> 71f3b86cdd819bdf11f39a522cdf59db541c47ba
 
 @NgModule({
   declarations: [
@@ -23,14 +19,10 @@ import { GuidelinesComponent } from './guidelines/guidelines.component';
     CalendarComponent,
     EventListComponent,
     CreateModalComponent,
-<<<<<<< HEAD
     WarnModalComponent,
     EventsComponent,
     NewComponent,
     GuidelinesComponent
-=======
-    WarnModalComponent
->>>>>>> 71f3b86cdd819bdf11f39a522cdf59db541c47ba
   ],
   imports: [
     BrowserModule,
